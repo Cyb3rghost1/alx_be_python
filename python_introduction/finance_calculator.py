2735794507e2515c1a83252537e8b6f9fd45bd68
Oshoba Lawrence <lawrenceoshoba@gmail.com>
2025-05-23 15:36:18 +0100

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,7 +4,7 @@ monthlyExpenses= int(input("Enter your total monthly expenses: "))
 monthlySavings = monthlyIncome - monthlyExpenses
 
 annualInterestRate =0.05
-projectedSavings = int(monthlySavings * 12 + (monthlySavings * 12 * annualInterestRate))
+projectedSavings = monthlySavings * 12 + (monthlySavings * 12 * annualInterestRate)
 PS = f"${projectedSavings}."
 MS = f"${monthlySavings}."
 print("Your monthly savings are", MS) 
