5c3d2f1ca56da1b255de99e0e3a45db5fe55c36e
Oshoba Lawrence <lawrenceoshoba@gmail.com>
2025-05-23 12:41:01 +0100

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+print("The addition of", number1, "and", number2, "is", addition)
+print("The subtraction of", number1, "and", number2, "is", subtraction)
+print("The multiplication of", number1, "and", number2, "is", multiplication)
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+hours = 2
+secondsInMinute = 60
+minuteInHour = 60
+
+secondsInTwoHours = hours * secondsInMinute * minuteInHour
+
+print ("2 hour(s) is", secondsInTwoHours, "seconds")
