b1a41b06d6967bccc06a937ffad4a84ef5942e06
Oshoba Lawrence <lawrenceoshoba@gmail.com>
2025-06-08 22:52:46 +0100

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,22 +3,22 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    return FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    return  (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR 
 
 def convert_to_fahrenheit(celsius):
-    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR ) + 32
 
 # Get user input
 try:
     temp = float(input("Enter the temperature to convert: "))
-    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
     
     if unit == "C":
-        converted_temp = convert_to_fahrenheit(temp)
-        print(f"{temp}°C is {converted_temp}°F")
+        converted = convert_to_fahrenheit(temp)
+        print(f"{temp}°C is {converted}°F")
     elif unit == "F":
-        converted_temp = convert_to_celsius(temp)
-        print(f"{temp}°F is {converted_temp}°C")
+        converted = convert_to_celsius(temp)
+        print(f"{temp}°F is {converted}°C")
     else:
         print("Invalid unit. Please enter C or F.")
 except ValueError:
