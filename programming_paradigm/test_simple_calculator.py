369b1be57fca0a232d888d16c5939d11cbe34caa
Oshoba Lawrence <lawrenceoshoba@gmail.com>
2025-06-15 16:35:31 +0100

    Add files via upload

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,31 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    
+    def setUp(self):
+        self.calc = SimpleCalculator()
+    
+    def test_add(self):
+        self.assertEqual(self.calc.add(3, 5), 8)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
+    
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(1, 5), -4)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 5), 15)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertIsNone(self.calc.divide(5, 0))
+
+if __name__ == '__main__':
+    unittest.main()
